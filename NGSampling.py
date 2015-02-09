b84a3b91ab5b71a41b24b479f20fb4c758f39326
Hadrien Gourlé <gourlehadrien@gmail.com>
2015-02-09 13:30:25 +0100

    pep8 NGSampling

--- NGSampling.py
+++ NGSampling.py
@@ -1,33 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-##### Written by Hadrien Gourlé. Feel free to use and modify #####
-Description="Reservoir sampling for NGS Data."
+"""Written by Hadrien Gourlé. Feel free to use and modify"""
+
 import random
 import argparse
 
-parser=argparse.ArgumentParser(description=Description)
-parser.add_argument("infile",help="Input file", metavar="[file.fastq]")
-parser.add_argument("--outfile","-o",default="sample.fastq",help="Output File (default: %(default)s)")
-parser.add_argument("--size","-s",type=int,default=10,help="Desired size of the sample (default: %(default)s)", metavar="[int]")
+Description = "Reservoir sampling for NGS Data."
+parser = argparse.ArgumentParser(description=Description)
+parser.add_argument(
+					"infile",
+					help="Input file",
+					metavar="[file.fastq]"
+					)
+parser.add_argument(
+					"--outfile",
+					"-o",
+					default="sample.fastq",
+					help="Output File (default: %(default)s)"
+					)
+parser.add_argument(
+					"--size",
+					"-s",
+					type=int,
+					default=10,
+					help="Desired size of the sample (default: %(default)s)",
+					metavar="[int]"
+					)
 
-args=parser.parse_args()
-Infile=open(args.infile,"r")
-Outfile=open(args.outfile,"w")
-Size=args.size
-Samples=[]
+args = parser.parse_args()
+Infile = open(args.infile, "r")
+Outfile = open(args.outfile, "w")
+Size = args.size
+Samples = []
 
 random.seed(1.2345)
-Nreads=sum(1 for _ in Infile)/4
-Samples=sorted([random.randint(0,Nreads-1) for s in xrange(Size)])
+Nreads = sum(1 for _ in Infile)/4
+Samples = sorted([random.randint(0, Nreads-1) for s in xrange(Size)])
 
-x=0
-Infile=open(args.infile)
+x = 0
+Infile = open(args.infile)
 for sample in sorted(Samples):
 	while x < sample:
-		x+=1
-		for i in range(4):Infile.readline()
-	for i in range(4):Outfile.write(Infile.readline())
-	x+=1
+		x += 1
+		for i in range(4):
+			Infile.readline()
+	for i in range(4):
+		Outfile.write(Infile.readline())
+	x += 1
 Infile.close()
 Outfile.close()
